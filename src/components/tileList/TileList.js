462539c2a796cd8266154dcab7684bcbf8621c0a
Bea Pan <bpankowska@gmail.com>
2023-06-26 15:41:33 +0300

    appointment page

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,10 +1,15 @@
 import React from "react";
-import {Tile} from '../../components/tile/Tile'
+import {Tile} from '../tile/Tile'
 
-export const TileList = (array) => {
+export const TileList = ({tiles}) => {
   return (
     <div>
-      <Tile />
+      {tiles.map((tile, index) => {
+        const {name, ...description } = tile;
+        return <Tile key={index} name={name} description={description}/>
+    })}
     </div>
   );
 };
+
+  
